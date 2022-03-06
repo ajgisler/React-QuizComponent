227d16e9088a73df5089da95b65afb0acc02650e
Andrew Gisler <ajgisler@outlook.com>
2022-03-06 15:15:45 -0600

    5.1

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
-import QuizQuestionButton from './QuizQuestionButton'
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
-    render() {
-        return ( <main>
-            <section>
-              <p>{this.props.quiz_question.instruction_text}</p>
-            </section>
-            <section className="buttons">
-              <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>    
-            </ul>
-            </section>
-          </main>)
-    }
+  render() {
+    return (
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.props.quiz_question.map((index) => (
+              <QuizQuestionButton
+                button_text={this.props.quiz_question.answer_options[index]}
+              />
+            ))}
+          </ul>
+        </section>
+      </main>
+    );
+  }
 }
 
-export default QuizQuestion
+export default QuizQuestion;

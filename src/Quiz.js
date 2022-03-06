bddf79a202353bcb3708cb166149ead1c4c104e7
Andrew Gisler <ajgisler@outlook.com>
2022-03-06 15:01:19 -0600

    4.8

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,16 +9,19 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
   render() {
+    const isQuizEnd =
+      (this.state.quiz_position = 1) === quizData.quiz_questions.length;
     return (
       <div>
-        if ({QuizEnd.isQuizEnd}) {<QuizEnd />} else
-        {
+        {isQuizEnd ? (
+          <QuizEnd />
+        ) : (
           <QuizQuestion
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
           />
-        }
+        )}
       </div>
     );
   }
